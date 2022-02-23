ae09422d4d0e4904e7a8322de17925fd24865699
grandzero <bayramutkuuzunlar@gmail.com>
2022-02-23 14:08:47 +0300

    compare gas usages of different version of storing array

--- assignment0/contracts/MultipleStoringTest.sol
+++ assignment0/contracts/MultipleStoringTest.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-contract StoreAddress {
+contract MultipleStoringTest {
     address[] public first;
     mapping(address => bool) public second;
     mapping(address => bool) public third;
---
+++ assignment0/test/multipleStoring.test.js
@@ -0,0 +1,76 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Multiple Storing Tests", function () {
+  let mst;
+  let addressList;
+  let gasResults=[];
+
+  beforeEach(async function(){
+    const MST = await ethers.getContractFactory("MultipleStoringTest");
+    mst = await MST.deploy();
+    await mst.deployed();
+
+    // Signer address objects
+    let [owner,...addrObjects] = await ethers.getSigners();
+    
+    // We only need the public addresses 
+    addressList = addrObjects.map(addr => addr.address);
+
+  });
+
+  it("Should save address array directly to storage", async function () {
+    // This operation calls SSTORE opcode
+    // Which is pretty expensive
+    let storeArrayTx = await mst.f1_storeAddr(addressList);
+
+    // Wait for TX resolve
+    const receipt = await storeArrayTx.wait();
+    const gasUsed = receipt.cumulativeGasUsed.toString() ;
+    gasResults.f1_storeAddr = gasUsed;
+
+    // Test wheter addresses saved
+    let savedAddr = await mst.first(0);
+    expect(savedAddr).to.equal(addressList[0]);
+
+  });
+
+  it("Should save addresses to mapping by looping", async function () {
+    
+    // This function simply iterates through all addresses
+    // And saves all addresses to a mapping
+    let storeMapping = await mst.f2_saveWithLoop(addressList);
+
+    // Wait for TX resolve
+    const receipt = await storeMapping.wait();
+    const gasUsed = receipt.cumulativeGasUsed.toString() ;
+    gasResults.f2_saveWithLoop = gasUsed;
+
+    // Test wheter addresses saved
+    let boolValue = await mst.second(addressList[0]);
+    expect(boolValue).to.equal(true);
+
+  });
+
+  it("Should save addresses to mapping by looping with inline assembly", async function () {
+    
+    // This function simply iterates through all addresses
+    // And saves all addresses to a mapping
+    let storeMapping = await mst.f3_loopWithInlineAssembly(addressList);
+
+    // Wait for TX resolve
+    const receipt = await storeMapping.wait();
+    const gasUsed = receipt.cumulativeGasUsed.toString() ;
+    gasResults.f3_loopWithInlineAssembly = gasUsed;
+
+    // Test wheter addresses saved
+    let boolValue = await mst.third(addressList[0]);
+    expect(boolValue).to.equal(true);
+
+  });
+
+  it("Compare all gas usages", function(){
+        console.table(gasResults);
+  });
+
+});
