59d54510ed6f378a48aed33da78c79b639e87234
grandzero <bayramutkuuzunlar@gmail.com>
2022-02-24 12:26:59 +0300

    add new tests to ballot

--- assignment0/test/4_ballotExtended.test.js
+++ assignment0/test/4_ballotExtended.test.js
@@ -9,6 +9,7 @@ describe("Extended Ballot Contract", function () {
   let tree; // Tree object
   let root; // Root hash
   let gasUsage = {};
+  let owner,addr1,addr2,addr3,addr4,addr5,addr6,addrObjects;
   beforeEach(async function(){
     const BallotExtended = await ethers.getContractFactory("BallotExtended");
     //let bytes32 = ethers.utils.formatBytes32String(text)
@@ -20,7 +21,7 @@ describe("Extended Ballot Contract", function () {
     await ballot.deployed();
 
 
-    let [owner, addr1, addr2, addr3, addr4, addr5, addr6, ...addrObjects] = await ethers.getSigners();
+    [owner, addr1, addr2, addr3, addr4, addr5, addr6, ...addrObjects] = await ethers.getSigners();
     addresses = addrObjects.map(x => x.address);
     leaves = addresses.map(x => keccak256(x));
     tree = new MerkleTree(leaves, keccak256, { sort: true })
@@ -41,11 +42,38 @@ describe("Extended Ballot Contract", function () {
 
   
   it("Should give vote right to one address", async function () {
+    let tx = await ballot.giveRightToVote(addr1.address);
+    
+    let receipt = await tx.wait();
+    const gasUsed = receipt.cumulativeGasUsed.toString();
+    gasUsage.giveRightToVote = gasUsed;
 
+    let seeChangeTx = await ballot.voters(addr1.address);
+    expect(seeChangeTx.weight).to.equal(1);
   });
 
   
   it("Should give batch vote right and should two address use vote successfully", async function () {
+    let tx = await ballot.setVerifiedHashRoot(root);
+    let proof1 = tree.getHexProof(keccak256(addresses[0]));
+    let proof2 = tree.getHexProof(keccak256(addresses[1]));
+    await tx.wait();
+    // let receipt = await tx.wait();
+    // const gasUsed = receipt.cumulativeGasUsed.toString();
+    // gasUsage.setVerifiedHashRoot = gasUsed;
+
+    tx = await ballot.connect(addrObjects[0]).vote(1,proof1);
+    let receipt = await tx.wait();
+    let gasUsed = receipt.cumulativeGasUsed.toString();
+    gasUsage.vote_1 = gasUsed;
+
+    tx = await ballot.connect(addrObjects[1]).vote(1,proof2);
+    receipt = await tx.wait();
+    gasUsed = receipt.cumulativeGasUsed.toString();
+    gasUsage.vote_2 = gasUsed;
+
+    let result = await ballot.proposals(1);
+    expect(result.voteCount).to.equal(2);
 
   });
 
